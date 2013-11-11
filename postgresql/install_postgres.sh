155d4cb4c0fe27591b81480e7ac92f1f5ef7e75c
Jordan Bach <jordanbach@gmail.com>
2013-11-11 10:07:45 -0600

    remove some unused files

--- postgresql/install_postgres.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-apt-get -yq install python-software-properties software-properties-common
-add-apt-repository ppa:pitti/postgresql
-apt-get -yq update
-apt-get -yq install postgresql-9.2 postgresql-client-9.2 postgresql-contrib-9.2
--- postgresql/start_postgres.sh
+++
@@ -1,1 +0,0 @@
-su postgres -c "/usr/lib/postgresql/9.2/bin/postgres -D /var/lib/postgresql/9.2/main -c config_file=/etc/postgresql/9.2/main/postgresql.conf"
